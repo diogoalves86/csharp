04bbf6ecfbf1b7bb3e9df4787de6aaae840f1ffe
Diogo Alves <diogoalvessilva86@gmail.com>
2016-09-01 16:42:06 -0300

    Brincando com o Mono... é foda!

---
+++ teste-mono/Teste/Teste/AdicionaEvento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teste
+{
+	public partial class AdicionaEvento : Gtk.Window
+	{
+		public AdicionaEvento () :
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
---
+++ teste-mono/Teste/Teste/MainWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+using Teste;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void btnAddEventoClicado (object sender, EventArgs e)
+	{
+		AdicionaEvento janelaAdicionaEvento = new AdicionaEvento ();
+		janelaAdicionaEvento.Show ();
+	}
+
+	protected void SelecionaDiaCalendario (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+}
